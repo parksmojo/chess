da1f15d3e1fb1242d9ca1c06fc89f112a592011c
pshum <parkersam@gmail.com>
2024-02-27 02:44:07 -0700

    Added create game tests

--- server/src/test/java/passoffTests/serverTests/ServiceUnitTests.java
+++ server/src/test/java/passoffTests/serverTests/ServiceUnitTests.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import passoffTests.testClasses.TestException;
 import model.*;
+import service.GameService;
 import service.UserService;
 
 public class ServiceUnitTests {
     UserService userService = new UserService();
+    GameService gameService = new GameService();
     UserData registeredUser = new UserData("ryguy", "pass", "ry@gmail.com");
     UserData otherUser = new UserData("jon3", "12345", "jon@gmail.com");
 
@@ -90,5 +92,52 @@ public class ServiceUnitTests {
         boolean success = userService.logout(auth.authToken());
 
         Assertions.assertFalse(success, "Repeated successful logouts");
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Create Game")
+    public void createGameSuccess() throws TestException {
+        String username = registeredUser.username();
+        String password = registeredUser.password();
+
+        AuthData auth = userService.login(username,password);
+
+        int gameID = gameService.makeGame(auth.authToken(), "My Game");
+
+        Assertions.assertTrue(gameID >= 0, "Invalid gameID given");
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Create more games")
+    public void createMoreSuccess() throws TestException {
+        String username = registeredUser.username();
+        String password = registeredUser.password();
+
+        AuthData auth = userService.login(username,password);
+
+        int gameID1 = gameService.makeGame(auth.authToken(), "My Game1");
+        int gameID2 = gameService.makeGame(auth.authToken(), "My Game2");
+        int gameID3 = gameService.makeGame(auth.authToken(), "My Game3");
+        int gameID4 = gameService.makeGame(auth.authToken(), "My Game4");
+
+        Assertions.assertTrue((gameID1 >= 0) && (gameID2 >= 0) && (gameID3 >= 0) && (gameID4 >= 0), "Invalid gameID given");
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Duplicate Game")
+    public void createGameFail() throws TestException {
+        String username = registeredUser.username();
+        String password = registeredUser.password();
+
+        AuthData auth = userService.login(username,password);
+
+        int gameID1 = gameService.makeGame(auth.authToken(), "My Game6");
+        int gameID2 = gameService.makeGame(auth.authToken(), "My Game6");
+
+        Assertions.assertTrue(gameID1 >= 0, "Invalid gameID given");
+        Assertions.assertTrue(gameID2 < 0, "Duplicate game allowed");
     }
 }
