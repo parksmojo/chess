b2b04f99c9a7808729daa655e998dc10fa98154f
pshum <parkersam@gmail.com>
2024-02-26 21:46:34 -0700

    Reworked gameDAO to use a HashSet

--- server/src/main/java/dataAccess/MemoryGameDAO.java
+++ server/src/main/java/dataAccess/MemoryGameDAO.java
@@ -1,9 +1,10 @@
 package dataAccess;
 
-import java.util.Vector;
+import model.GameData;
+import java.util.HashSet;
 
 public class MemoryGameDAO implements GameDAO {
-    private final Vector games = new Vector<>();
+    private HashSet<GameData> games = new HashSet<>();
 
     public void clear(){
         games.clear();
