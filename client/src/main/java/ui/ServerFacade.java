56031ef495629b52f0108edb30170abb0be5e622
Parker <parkersam@gmail.com>
2024-03-11 21:36:20 -0600

    Added starter classes

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class ServerFacade {
+    // This will have a method for each endpoint
+    // ie Login, Register, etc.
+    // they will use HTTP connection and return an object
+}
