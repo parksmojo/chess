2a07304f9566df45b18a17df7865275d11d6565e
Parker <parkersam@gmail.com>
2024-03-12 22:33:31 -0600

    Implemented facade register method - Added helper methods to facilitate creation of other methods

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,14 +1,80 @@
 package ui;
 
-public class ServerFacade {
-    // This will have a method for each endpoint
-    // ie Login, Register, etc.
-    // they will use HTTP connection and return an object
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ServerFacade {
+    private static String serverURL = "http://localhost:";
+
+    public ServerFacade(int port) {
+        serverURL += Integer.toString(port);
+    }
+
+    public AuthData register(String username, String password, String email) throws Exception {
+        String path = "/user";
+        UserData user = new UserData(username,password,email);
+        return this.makeRequest("POST",path,user,AuthData.class);
+    }
     public static Object login(){
         return null;
     }
-    public static Object register(){
-        return null;
+
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverURL + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
     }
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            throw new ResponseException(status, "failure: " + status);
+        }
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
 }
